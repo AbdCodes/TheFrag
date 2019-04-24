171c49543e183bc0e393349d102fef68286aa979
AbdCodes <abdullah.miyan9848@gmail.com>
2019-04-25 02:18:38 +0545

    Initial commit

---
+++ app/src/main/java/com/e/thefrag/MainActivity.java
@@ -0,0 +1,50 @@
+package com.e.thefrag;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import fragments.FirstFragment;
+import fragments.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnFragment;
+    private Boolean status=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment=findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=((FragmentManager) fragmentManager).beginTransaction();
+
+
+        if (status){
+            FirstFragment firstFragment=new FirstFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("load Second Fragment");
+            status=false;
+        }
+
+        else{
+            SecondFragment secondFragment=new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("load First Fragment");
+            status=true;
+        }
+    }
+}
